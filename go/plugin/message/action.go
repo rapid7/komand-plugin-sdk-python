ecc1b2347d0d6f41d26fb8f2a10941db18b6677b
Jim Salem <jsalem@backupify.com>
2016-05-09 16:19:03 -0400

    Fix status type

--- go/plugin/message/action.go
+++ go/plugin/message/action.go
@@ -12,7 +12,7 @@ type ActionStart struct {
 // ActionResult is the format of the message from an Actions result
 type ActionResult struct {
 	Meta   *json.RawMessage `json:"meta"`
-	Status string           `json:"status"` // Status identifies the result status from the Action
+	Status StatusType       `json:"status"` // Status identifies the result status from the Action
 	Error  string           `json:"error"`  // Error identifies any error that occured during the Action
 	Output OutputMessage    `json:"output"` // Output contains the output of the Action
 }
