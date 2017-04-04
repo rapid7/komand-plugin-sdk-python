47541e9118a6c285af6e3282d140570e572824f6
Sean Kelly <sean@komand.com>
2017-04-04 09:35:31 -0400

    Apparently my linter and the CI linter expect different things - fun

--- komand/cli.py
+++ komand/cli.py
@@ -95,7 +95,7 @@ class CLI(object):
             for name, item in self.plugin.actions.items():
                 result += ('└── %s%s%s (%s%s)\n') % (GREEN, name, RESET, item.description, RESET)
 
-        print result
+        print(result)
 
     def _run(self, args):
         if args.debug:
