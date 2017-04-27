b38f6461571e99e8d6379f0574a390f8f752c1de
Sean Kelly <sean@komand.com>
2017-04-27 16:49:37 -0400

    no more global flask

--- komand/server.py
+++ komand/server.py
@@ -6,41 +6,39 @@ import komand.message as message
 import komand.action
 import komand.dispatcher
 
-# this is global...
-# there can only be one app in python servers i guess.
-# I don't know. :shrug:
-# lol python
-app = Flask(__name__)
+def create_flask_app():
+    app = Flask(__name__)
 
-@app.route('/<string:prefix>/<string:name>', defaults={'test': None}, methods=['POST'])
-@app.route('/<string:prefix>/<string:name>/<string:test>', methods=['POST'])
-def handler(prefix, name, test):    
-    msg = request.get_json()
-    if request.method == 'POST':
-        dummy = request.form
-    if not g.control:
-        logging.fatal("Fatal error - no control server provided")
+    @app.route('/<string:prefix>/<string:name>', defaults={'test': None}, methods=['POST'])
+    @app.route('/<string:prefix>/<string:name>/<string:test>', methods=['POST'])
+    def handler(prefix, name, test):    
+        msg = request.get_json()
+        if request.method == 'POST':
+            dummy = request.form
+        if not g.control:
+            logging.fatal("Fatal error - no control server provided")
 
-    is_test = test is not None
-    logging.error('request json: %s', msg)
-    result = {}
-    # TODO: wrap in antoher try/catch and capture any errors
-    if prefix == "actions":
-        result = g.control.handle_action(name, msg, is_test)
-    elif prefix == "triggers" and is_test:
-        result = g.control.handle_trigger(name, msg)
-    else:
-        # It wasn't an action, and it wasn't a trigger test, so it was a trigger non test
-        # Not supported, return error
-        logging.fatal("Fatal error - must specify either actions or triggers in the url")
-        response = jsonify(message.TriggerEvent(meta={}, output={}))
-        response.status_code = 400
+        is_test = test is not None
+        logging.error('request json: %s', msg)
+        result = {}
+        # TODO: wrap in antoher try/catch and capture any errors
+        if prefix == "actions":
+            result = g.control.handle_action(name, msg, is_test)
+        elif prefix == "triggers" and is_test:
+            result = g.control.handle_trigger(name, msg)
+        else:
+            # It wasn't an action, and it wasn't a trigger test, so it was a trigger non test
+            # Not supported, return error
+            logging.fatal("Fatal error - must specify either actions or triggers in the url")
+            response = jsonify(message.TriggerEvent(meta={}, output={}))
+            response.status_code = 400
+            return response
+        response = jsonify(result)
+        if 'plugin_error' in result:
+            response.status_code = 500
+
         return response
-    response = jsonify(result)
-    if 'plugin_error' in result:
-        response.status_code = 500
-
-    return response
+    return app
 
 class Server(object):
     """Server runs the plugin in server mode"""
@@ -48,13 +46,14 @@ class Server(object):
         self.plugin = plugin
         self.port = port
         self.debug = debug
+        self.app = create_flask_app()
 
     def start(self):
         """ start server """
-        with app.app_context():
+        with self.app.app_context():
             g.control = self
             logging.info("Starting server on port: %d", self.port)
-            app.run(host='0.0.0.0', port=self.port)
+            self.app.run(host='0.0.0.0', port=self.port)
 
     def handle_action(self, name, msg, is_test):
         """Run handler"""
