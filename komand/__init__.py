457f2b8374c155be37581b7d207205c8b86b523d
Mike Rinehart <mike_rinehart@rapid7.com>
2017-10-27 16:24:05 -0500

    Remove commented lines in __init__

--- komand/__init__.py
+++ komand/__init__.py
@@ -1,16 +1,1 @@
 __all__ = ['message', 'plugin', 'connection', 'trigger', 'action', 'variables', 'cli', 'helper']
-
-# import komand.plugin
-# 
-# import komand.action
-# import komand.trigger 
-# import komand.connection
-# import komand.cli
-# 
-# Plugin = komand.plugin.Plugin
-# Action = komand.action.Action
-# Trigger = komand.trigger.Trigger
-# Connection = komand.connection.Connection
-# Input = komand.variables.Input
-# Output = komand.variables.Output
-# CLI = komand.cli.CLI
