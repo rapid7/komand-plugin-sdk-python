364b9bee6d27bad03a56811aaf7033341df2d775
Mike Rinehart <mike_rinehart@rapid7.com>
2017-10-29 22:47:39 -0500

    Fix tests message tests for python2 and 3

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,5 +1,6 @@
 import unittest
 from io import StringIO
+from io import BytesIO
 import sys, os
 import komand.message
 
@@ -9,14 +10,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class TestMessage(unittest.TestCase):
 
   def test_ok_message(self):
-      sample = '{ "meta": { "test": "this" }, "body": {"trigger": "hello123"}, "version": "v1", "type": "trigger_start" }'
+      sample = u'{ "meta": { "test": "this" }, "body": {"trigger": "hello123"}, "version": "v1", "type": "trigger_start" }'
       fd = StringIO(sample)
       m = komand.message.unmarshal(fd)
       print(m)
 
 
   def test_invalid_message_fails(self):
-      sample = '{ "hello": "there" }'
+      sample = u'{ "hello": "there" }'
       fd = StringIO(sample)
       with self.assertRaises(Exception):
         komand.message.unmarshal(fd)
