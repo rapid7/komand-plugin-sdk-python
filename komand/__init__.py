b00e9213335c2465f726ccfa329ba4ce7c098177
mikeatlas-r7 <mike_atlas@rapid7.com>
2017-12-11 18:12:48 -0500

    SSL_CERT_FILE works, but SSL_CERT_DIR doesn't. Le Sigh

--- komand/__init__.py
+++ komand/__init__.py
@@ -16,11 +16,11 @@ Output = komand.variables.Output
 CLI = komand.cli.CLI
 
 # Many plugins use the certifi package, particularly indirectly through
-# the `requests` package. Certifi can be monkey-patched to not use the 
+# the requests package. Certifi can be monkey-patched to not use the 
 # dedicated CA bundle, which is exactly what we will do. In fact, the 
-# `requests` package even suggests to do exactly this: 
+# requests package even suggests to do exactly this: 
 # https://github.com/requests/requests/blob/master/requests/certs.py
-# So, we'll ask it to use SSL_CERT_DIR, one of the most common env vars
+# So, we'll ask it to use SSL_CERT_FILE, one of the most common env vars
 # that would contain a path to SSL CA certificate directory.
 # We'll also set REQUESTS_CA_BUNDLE if it wasn't set already.
 import certifi
@@ -29,7 +29,7 @@ old_certifi_value = certifi.where()
 
 def where():
     try:
-        env_var = os.environ['SSL_CERT_DIR']
+        env_var = os.environ['SSL_CERT_FILE']
         if len(env_var) > 0 and os.path.exists(env_var):
             # tell the requests package to use it too
             os.environ['REQUESTS_CA_BUNDLE'] = env_var
