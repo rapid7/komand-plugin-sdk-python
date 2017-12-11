91c86837526ce52dc4a55bdf5e8e5cf1851a71f5
Mike Rinehart <mike_rinehart@rapid7.com>
2017-12-11 15:17:02 -0600

    Remove embedded JSON schema from connection

--- komand/connection.py
+++ komand/connection.py
@@ -1,30 +1,10 @@
-from jsonschema import validate
-import komand.util as util
-import komand.helper as helper
-
 class Connection(object):
     """Komand connection"""
-    def __init__(self, schema={}):
-        self.schema = schema 
+
+    def __init__(self, input):
+        self.schema = input.schema
         self.parameters = {}
-
-    def set(self, parameters):
-        """ Set parameters """
-        self.parameters = parameters
-        self._validate()
-
-    def _validate(self):
-        """ Validate variables """
-        if self.schema:
-            validate(self.parameters, self.schema)
 
     def connect(self, params={}):
         """ Connect """
         raise NotImplementedError
-
-    def sample(self):
-        """ Sample object """
-        if self.schema:
-            return util.sample(self.schema)
-    
-
