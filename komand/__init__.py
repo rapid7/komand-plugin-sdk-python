17c7a18d3c0962770c6ac6afbc55e60d76bc816f
Mike Rinehart <mike_rinehart@rapid7.com>
2017-12-11 16:54:41 -0600

    Change imports

--- komand/__init__.py
+++ komand/__init__.py
@@ -6,11 +6,10 @@ import komand.action
 import komand.trigger 
 import komand.connection
 import komand.cli
+from komand.variables import Input, Output
 
 Plugin = komand.plugin.Plugin
 Action = komand.action.Action
 Trigger = komand.trigger.Trigger
 Connection = komand.connection.Connection
-Input = komand.variables.Input
-Output = komand.variables.Output
 CLI = komand.cli.CLI
