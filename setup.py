8cd37a7b408857aa459272b2172d36874286ce85
Jacob Rickerd <jacobrickerd@gmail.com>
2018-02-22 08:48:29 -0500

    Incremented Version Number to 0.3.14

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='komand',
-      version='0.3.13',
+      version='0.3.14',
       description='Komand Plugin SDK',
       author='Komand',
       author_email='support@komand.com',
