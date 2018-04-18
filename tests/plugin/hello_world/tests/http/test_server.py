e25929861e7acfdf88cde6191503b257d2c90f38
Matthew Broomfield <mbroomfield@rapid7.com>
2018-04-18 17:31:48 -0400

    I was chasing a bug but I cant find it

--- tests/plugin/hello_world/tests/http/test_server.py
+++ tests/plugin/hello_world/tests/http/test_server.py
@@ -40,14 +40,19 @@ def test_404(plugin_pid):
     assert requests.post('http://localhost:10001').status_code == 404
 
 
-def run_action(input_file, output_file, expect_code=200):
+def run_action(input_file, output_file, expect_code=200, is_test=False):
 
     input_message = json.load(open(input_file))
     expected_output = json.load(open(output_file))
 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    response = requests.post('http://localhost:10001/{}/{}'.format('action', input_message['body']['action']),
-                             json=input_message, headers=headers)
+
+    if is_test:
+        url = 'http://localhost:10001/{}/{}/test'.format('action', input_message['body']['action'])
+    else:
+        url = 'http://localhost:10001/{}/{}'.format('action', input_message['body']['action'])
+
+    response = requests.post(url, json=input_message, headers=headers)
     assert response.status_code == expect_code
 
     output = response.json()
@@ -76,3 +81,17 @@ def test_server_bad_json_action():
 
     run_action('./tests/plugin/hello_world/tests/action/return_bad_json/input.json',
                './tests/plugin/hello_world/tests/action/return_bad_json/output.json', expect_code=500)
+
+
+def test_server_test_simple_action():
+    run_action('./tests/plugin/hello_world/tests/action/hello/input.json',
+               './tests/plugin/hello_world/tests/action/hello/test_output.json', expect_code=200, is_test=True)
+
+
+def test_server_test_throw_exeception_action():
+
+    if six.PY2:
+        return
+
+    run_action('./tests/plugin/hello_world/tests/action/throw_exception/input.json',
+               './tests/plugin/hello_world/tests/action/throw_exception/test_output.json', expect_code=500, is_test=True)
