a63bee2a40fd516a2a33f2db65e4b7523e8f5d46
Mike Rinehart <mike_rinehart@rapid7.com>
2018-11-05 13:57:31 -0500

    Add ConnectionTestException

--- komand/exceptions.py
+++ komand/exceptions.py
@@ -30,3 +30,47 @@ class LoggedException(Exception):
         super(LoggedException, self).__init__(ex)
         self.ex = ex
         self.output = output
+
+
+class ConnectionTestException(BaseException):
+    """
+    An Exception which marks an error that occurred during a connection test.
+
+    This Exception provides a method for consistent and well-handled error messaging.
+    """
+
+    class Preset(object):
+        API_KEY = "api_key"
+        UNAUTHORIZED = "unauthorized"
+        RATE_LIMIT = "rate_limit"
+
+    causes = {
+        Preset.API_KEY: "Invalid API key used.",
+        Preset.UNAUTHORIZED: "The account configured in your plugin connection is unauthorized to access this service.",
+        Preset.RATE_LIMIT: "The account configured in your plugin connection is currently rate-limited.",
+    }
+
+    assistances = {
+        Preset.API_KEY: "Verify your API key configured in your connection is correct.",
+        Preset.UNAUTHORIZED: "Verify the permissions for your account and try again.",
+        Preset.RATE_LIMIT: "Adjust the time between requests in the plugin action configuration if possible or "
+                           "consider adding a Sleep plugin step between attempts.",
+    }
+
+    def __init__(self, cause=None, assistance=None, preset=None):
+        """
+        Initializes a new ConnectionTestException. User must supply all punctuation/grammar.
+        :param cause: Cause of the error. Leave empty if using preset.
+        :param assistance: Possible remediation steps for the error. Leave empty if using preset.
+        :param preset: Preset error and remediation steps to use.
+        """
+        
+        if preset:
+            self.cause, self.assistance = self.causes[preset], self.assistances[preset]
+        else:
+            self.cause = cause
+            self.assistance = assistance
+
+    def __str__(self):
+        return "Connection test failed! {cause} {assistance}".format(cause=self.cause,
+                                                                     assistance=self.assistance)
