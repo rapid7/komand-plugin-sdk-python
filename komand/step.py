dd5e47319d515490b835e9f0d85f68484958e140
Mike Rinehart <mike_rinehart@rapid7.com>
2018-12-20 12:31:30 -0600

    Remove code from step

--- komand/step.py
+++ komand/step.py
@@ -18,12 +18,6 @@ class Step(object):
         self.handler = logging.StreamHandler(self.stream)
         self.logger = logging.getLogger(self.name)
 
-        self.logger.info("{vendor}-{plugin_name}-{plugin_version}:{step_name}".
-                         format(vendor=self.connection.meta.vendor,
-                                plugin_name=self.connection.meta.name,
-                                plugin_version=self.connection.meta.version,
-                                step_name=self.name))
-
     def run(self, params={}):
         """ Run a action, return output or raise error """
         raise NotImplementedError
